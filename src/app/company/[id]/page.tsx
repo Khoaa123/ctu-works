e06ea912f903cce2c4e53dd228fe859efd31ecb4
Chuong <chuongvo900@gmail.com>
2024-09-19 02:17:28 +0700

    update company page

--- src/app/company/[id]/page.tsx
+++ src/app/company/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
-import React, { useRef, useState } from "react";
-import nexon from "@images/nexon.png";
+import React, { useEffect, useRef, useState } from "react";
+import logo from "@images/logo.png";
 import {
   Select,
   SelectContent,
@@ -13,7 +13,16 @@ import {
 } from "@/components/ui/select";
 import { GoSearch } from "react-icons/go";
 import ScrollToTopButton from "@/components/ScrollToTopButton/ScrollToTopButotn";
+import { useCookies } from "next-client-cookies";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
 
+export interface JwtPayload {
+  userid: string;
+  email: string;
+  fullName: string;
+  role: string;
+}
 const CompanyDetail = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const jobSectionRef = useRef<HTMLDivElement>(null);
@@ -27,6 +36,115 @@ const CompanyDetail = () => {
       jobSectionRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+  const [detailsCompany, setDetailsCompany] = useState({
+    companyLogo: '',
+    companyName: '',
+    companyScale: '',
+    companyIndustries: '',
+    fullName: '',
+    companyAddress: '',
+    companyDescription: '',
+    following: -1,
+    follower: []
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDetailsCompany();
+      setDetailsCompany(data.data);
+      // console.log(data.data);
+    };
+    fetchData();
+  }, []);
+  const fetchDetailsCompany = async () => {
+    const id = location.pathname.split('/company/')[1]
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/recruiter/get-details-recruiter/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return res.json();
+  };
+  const [jobPostCompany, setJobPostCompany] = useState([{
+    companyLogo: '',
+    companyName: '',
+    salary: '',
+    jobTitle: '',
+    location: [],
+    _id: ''
+  }]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchJobPostCompany();
+      setJobPostCompany(data.data);
+      console.log(data.data)
+    };
+    fetchData();
+  }, []);
+  const fetchJobPostCompany = async () => {
+    const id = location.pathname.split('/company/')[1]
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/jobpost/get-my-jobpost/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return res.json();
+  };
+
+  const cookies = useCookies();
+  const accessToken = cookies.get("accessToken");
+  const decodedToken = accessToken ? jwtDecode<JwtPayload>(accessToken) : null;
+
+  const addFollow = async () => {
+    const id = location.pathname.split('/company/')[1]
+    const userId = decodedToken?.userid
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/follow/create-follow/${id}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: userId
+        }),
+      }
+    );
+    return res.json();
+  };
+
+  const unFollow = async () => {
+    const id = location.pathname.split('/company/')[1]
+    const userId = decodedToken?.userid
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/follow/delete-follow/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          recruiterId: id,
+          userId: userId
+        }),
+      }
+    );
+    return res.json();
+  };
+  const router = useRouter();
+
+  const navigate = (id: string) => {
+    router.push(`/job/${id}`)
+  }
+
   return (
     <>
       <div className="bg-[#F1F2F4]">
@@ -34,17 +152,34 @@ const CompanyDetail = () => {
           <div className="pt-5">
             <div className="rounded-md bg-white px-12 py-6">
               <div className="flex justify-between gap-3">
-                <Image src={nexon} alt="company" height={100} width={100} />
+                <Image src={detailsCompany?.companyLogo || logo} alt="company-logo" height={100} width={100} />
                 <div className="ml-2 flex-1">
                   <p className="text-xl font-semibold">
-                    Nexon Networks Vina Co. Ltd,
+                    {/* Nexon Networks Vina Co. Ltd, */}
+                    {detailsCompany?.companyName}
                   </p>
-                  <p className="text-sm">234 lượt theo dõi</p>
+                  <p className="text-sm">
+                    {/* 234 lượt theo dõi */}
+                    {detailsCompany?.following || 0} lượt theo dõi
+
+                  </p>
                 </div>
                 <div>
-                  <button className="flex flex-1 items-center justify-center rounded-lg bg-[#ff7d55] px-4 py-2 text-sm text-white transition hover:bg-[#fd916f]">
-                    Theo dõi
-                  </button>
+                  {detailsCompany?.follower.some((item) => item === decodedToken?.userid) ? (
+                    <button
+                      className="flex flex-1 items-center justify-center rounded-lg bg-[#ff7d55] px-4 py-2 text-sm text-white transition hover:bg-[#fd916f]"
+                      onClick={unFollow}
+                    >
+                      Đang theo dõi
+                    </button>
+                  ) : (
+                    <button
+                      className="flex flex-1 items-center justify-center rounded-lg bg-[#ff7d55] px-4 py-2 text-sm text-white transition hover:bg-[#fd916f]"
+                      onClick={addFollow}
+                    >
+                      Theo dõi
+                    </button>
+                  )}
                 </div>
               </div>
               <div className="my-10">
@@ -64,31 +199,40 @@ const CompanyDetail = () => {
                 <p className="my-6 text-xl font-semibold">Về chúng tôi</p>
                 <div className="my-2 grid grid-cols-[100px_auto] gap-4 text-sm">
                   <p className="w-[100px]">Quy mô</p>
-                  <p>100 - 499 nhân viên</p>
+                  <p>
+                    {/* 100 - 499 nhân viên */}
+                    {detailsCompany?.companyScale}
+                  </p>
                 </div>
                 <div className="my-2 grid grid-cols-[100px_auto] gap-4 text-sm">
                   <p className="w-[100px]">Lĩnh vực</p>
-                  <p>Phần Mềm CNTT/Dịch vụ Phần mềm</p>
+                  <p>
+                    {/* Phần Mềm CNTT/Dịch vụ Phần mềm */}
+                    {detailsCompany?.companyIndustries}
+                  </p>
                 </div>
                 <div className="my-2 grid grid-cols-[100px_auto] gap-4 text-sm">
                   <p className="w-[100px]">Liên hệ</p>
-                  <p>Ms. Oanh</p>
+                  <p>
+                    {/* Ms. Oanh */}
+                    {detailsCompany?.fullName}
+                  </p>
                 </div>
                 <div className="my-2 grid grid-cols-[100px_auto] gap-4 text-sm">
                   <p className="w-[100px]">Địa chỉ</p>
                   <p>
-                    Tòa nhà UOA Tower, 06 Đường Tân Trào, Phường Tân Phú, Quận
-                    7, TP. Hồ Chí Minh
+                    {/* Tòa nhà UOA Tower, 06 Đường Tân Trào, Phường Tân Phú, Quận
+                    7, TP. Hồ Chí Minh */}
+                    {detailsCompany?.companyAddress}
                   </p>
                 </div>
                 <div
-                  className={` transition-all duration-400 ease-in-out overflow-hidden ${
-                    isExpanded ? "max-h-[1000px]" : "max-h-[170px]"
-                  }`}
+                  className={` transition-all duration-400 ease-in-out overflow-hidden ${isExpanded ? "max-h-[1000px]" : "max-h-[170px]"
+                    }`}
                 >
                   <div className="my-4">
                     <p className="text-sm">
-                      Nexon Networks VINA, responsible for the uppermost quality
+                      {/* Nexon Networks VINA, responsible for the uppermost quality
                       of services to our customers by specializing in game
                       quality assurance, was established in Ho Chi Minh, Vietnam
                       in the year 2019. As a proud member of the Nexon Company
@@ -98,18 +242,19 @@ const CompanyDetail = () => {
                       concentrate on the goal. The basis and strength behind our
                       rapid growth worldwide derive from our employees. We are
                       expecting passionate, self-challenging and eager
-                      individuals to apply and join our journey to success.
+                      individuals to apply and join our journey to success. */}
+                      {detailsCompany?.companyDescription}
                     </p>
                   </div>
-                  <div className="my-4">
+                  {/* <div className="my-4">
                     <p className="text-sm">
                       NEXON Group is a Global Company Group well known for
                       developing and servicing MapleStory, Dungeon & Fighter,
                       Sudden Attack, KartRider, FIFA ONLINE 4, V4, AXE, and over
                       60 other major titles in 190 countries worldwide.
                     </p>
-                  </div>
-                  <div className="my-4 text-sm">
+                  </div> */}
+                  {/* <div className="my-4 text-sm">
                     <p>We offer</p>
                     <ul className="">
                       <li>
@@ -143,7 +288,7 @@ const CompanyDetail = () => {
                         fairly, and trusted
                       </li>
                     </ul>
-                  </div>
+                  </div> */}
                 </div>
 
                 <button
@@ -182,7 +327,30 @@ const CompanyDetail = () => {
                     </Select>
                   </div>
                   <div className="mt-2 flex flex-col gap-4">
-                    <div className="flex items-center gap-5 rounded-md border border-solid border-gray-200 p-4 transition hover:border-sky-200 hover:bg-[#F9FBFF]">
+                    {jobPostCompany?.map((item, index) => (
+                      <div key={index} className="flex items-center gap-5 rounded-md border border-solid border-gray-200 p-4 transition hover:border-sky-200 hover:bg-[#F9FBFF]">
+                        <Image src={item?.companyLogo} alt="companyLogo" height={80} width={80} />
+                        <div>
+                          <h1 className="mb-1 line-clamp-1 text-xl font-bold"
+                            onClick={() => navigate(item?._id)}
+                          >
+                            {item?.jobTitle}
+                          </h1>
+                          <p>
+                            {item?.companyName}
+                          </p>
+                          <p className="my-1 text-sm text-amber-600">
+                            {item?.salary ?? 'Thương lượng'}
+                          </p>
+                          <p className="text-sm">
+                            {item?.location?.map((loc, locIndex) => (
+                              <p key={locIndex}>{loc}</p>
+                            ))}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                    {/* <div className="flex items-center gap-5 rounded-md border border-solid border-gray-200 p-4 transition hover:border-sky-200 hover:bg-[#F9FBFF]">
                       <Image src={nexon} alt="" height={80} width={80} />
                       <div>
                         <h1 className="mb-1 line-clamp-1 text-xl font-bold">
@@ -220,20 +388,7 @@ const CompanyDetail = () => {
                         </p>
                         <p className="text-sm">Hồ Chí Minh</p>
                       </div>
-                    </div>
-                    <div className="flex items-center gap-5 rounded-md border border-solid border-gray-200 p-4 transition hover:border-sky-200 hover:bg-[#F9FBFF]">
-                      <Image src={nexon} alt="" height={80} width={80} />
-                      <div>
-                        <h1 className="mb-1 line-clamp-1 text-xl font-bold">
-                          QA Game Tester
-                        </h1>
-                        <p>Nexon Networks Vina Co.Ltd</p>
-                        <p className="my-1 text-sm text-amber-600">
-                          Thương lượng
-                        </p>
-                        <p className="text-sm">Hồ Chí Minh</p>
-                      </div>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
